f0c45549a1e3e7a01c83729ea08ba63f1c234520
MD SALIM HOSSAIN <mdsalimhossain40476@gmail.com>
2024-04-07 00:48:10 +0600

    WriteLineWrite

---
+++ 1 Write Print Read/WriteLineWrite/WriteLineWrite/WriteLineWrite/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteLineWrite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am Learning C#");
+            Console.WriteLine("It is awesome!");
+            //আপনি যতগুলি চান ততগুলি WriteLine () পদ্ধতি যোগ করতে পারেন। লক্ষ্য করুন যে এটি প্রতিটি পদ্ধতির জন্য একটি নতুন লাইন যোগ করবে:
+            Console.WriteLine(3 + 3);
+            //আপনি সংখ্যাও পাঠাতে পারেন, এবং গাণিতিক গণনা করতে পারেন:
+
+            Console.Write("Hello World! ");
+            Console.Write("I will print on the same line.");
+            /*  এখানে একটি Write() পদ্ধতি রয়েছে, যা WriteLine() - এর অনুরূপ।
+                একমাত্র পার্থক্য হল এটি আউটপুটের শেষে একটি নতুন লাইন যুক্ত করে না।
+            */
+            Console.ReadLine();
+        }
+    }
+}
