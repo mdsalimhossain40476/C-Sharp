386e7df840657d639d42dfb8be54f8b29455c989
MD SALIM HOSSAIN <159735559+mdsalimhossain40476@users.noreply.github.com>
2024-04-07 11:22:09 +0600

    Variables

---
+++ 3 Variables/3_DisplayVariables/DisplayVariables/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               WriteLine() পদ্ধতিটি প্রায়ই কনসোল উইন্ডোতে ভেরিয়েবলের মান প্রদর্শন করতে ব্যবহৃত হয়।
+               
+               একটি ভেরিয়েবল এবং টেক্সট একত্রিত করতে, + অক্ষরটি ব্যবহার করুন:
+               
+            */
+            string name = "Md Salim Hossain";
+            Console.WriteLine("Hello " + name);   //  Output  :	Hello Md Salim Hossain
+
+            /*
+               আপনি + ক্যারেক্টারটি ব্যবহার করে একটি ভেরিয়েবলকে আরেকটি ভেরিয়েবলের সাথে যোগ করতে পারেন।
+            */
+            string firstName = "Md ";
+            string lastName = "Salim";
+            string fullName = firstName + lastName;
+            Console.WriteLine(fullName); //  Output: Md Salim
+
+            /*
+                সংখ্যার মানের জন্য, +চিহ্নটি গাণিতিক অপারেটর হিসাবে কাজ করে(লক্ষ্য করুন যে আমরা এখানে int(পূর্ণসংখ্যা) ভেরিয়েবল ব্যবহার করি):
+            */
+            int x = 10;
+            int y = 15;
+            Console.WriteLine(x + y);  //  Output  :	25
+            /*
+                  উপরের উদাহরণ থেকে, আপনি আশা করতে পারেন:
+               
+               x 5 মান সংরক্ষণ করে
+               y 6 মান সংরক্ষণ করে
+               তারপর আমরা x + y এর মান প্রদর্শন করতে WriteLine() পদ্ধতি ব্যবহার করি, যা 25 ।        
+               
+            */
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 3 Variables/4_MultiplyVariables/MultiplyVariables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare Many Variables
+            /*
+               একই ধরনের একাধিক ভেরিয়েবল ঘোষণা করতে, কমা-বিচ্ছিন্ন ( , ) তালিকা ব্যবহার করুন:
+            */
+            int x = 15, y = 20, z = 50;
+            Console.WriteLine(x + y + z);  //  Output  :	85
+
+            // একটি লাইনে একাধিক ভেরিয়েবলে একই মান অ্যাসাইন করতে পারেন:
+            int a, b, c;
+            a = b = c = 30;
+            Console.WriteLine(a + b + c);    // Output  :	90                                                                             
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
