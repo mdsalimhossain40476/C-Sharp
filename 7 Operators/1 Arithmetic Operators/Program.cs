4bdbddda90c4c9e366d743fbc2aeb6e278ca236a
MD SALIM HOSSAIN <159735559+mdsalimhossain40476@users.noreply.github.com>
2024-04-07 14:53:07 +0600

    Operators

---
+++ 7 Operators/1 Arithmetic Operators/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Arithmetic Operators
+            // গাণিতিক অপারেটর সাধারণ গাণিতিক কার্য সম্পাদনের জন্য ব্যবহৃত হয়:
+            
+            /*
+                  Operator	     Name	               Description	                    Example	
+
+                     +	       Addition	               দুটি মানকে একসাথে যোগ করে।	     x + y	            */               int x = 5;
+               int y = 3;
+               Console.WriteLine(x + y);  //  Output : 8
+
+            /*  
+              Operator        Name                  Description                     Example
+
+                 -	       Subtraction	           একটি মান থেকে অন্য মান বাদ দেয়।	 x - y                */
+            int a = 5;
+            int b = 3;
+            Console.WriteLine(a - b);  // Output :  2
+
+            /*
+               Operator        Name                  Description                     Example
+
+                 *	       Multiplication	      দুটি মানের গুণফল বের করে।	         x * y	           */
+            int c = 5;
+            int d = 3;
+            Console.WriteLine(c * d);   // Output :  15 
+
+            /*                   Operator        Name              Description                            Example
+
+                     /	         Division	         একটি মানকে অন্য একটি দ্বারা ভাগ করুন।   	 x / y	           */
+            int e = 12;
+            int f = 3;
+            Console.WriteLine(e / f);  //  Output :  4
+
+            /*                   Operator        Name                  Description                        Example
+                     %	         Modulus	             বিভাগের বাকি ফেরত দেয়।	             x % y	            */
+            int g = 5;
+            int h = 2;
+            Console.WriteLine(g % h);   //  Output :  1
+
+            /*                   Operator        Name                  Description                          Example
+                     ++	          Increment	          একটি ভেরিয়েবলের মান 1 দ্বারা বৃদ্ধি করে।	       x++	            */
+            int i = 5;
+            i++;
+            Console.WriteLine(i);  // Output :  6
+
+            /*                   Operator        Name                  Description                         Example
+                     --	          Decrement	           একটি ভেরিয়েবলের মান ১ দ্বারা হ্রাস করে।	 x--	               
+            */
+            int j = 5;
+                j--;
+            Console.WriteLine(j);   // Output :  4
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 7 Operators/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  C# Operators
+            //ভেরিয়েবল / পরিবর্তনশীল এবং মানের উপর অপারেশন করার জন্য অপারেটর ব্যবহার করা হয়।
+
+            // নিচের উদাহরণে, আমরা দুটি মানকে একত্রে যোগ করার জন্য +অপারেটর ব্যবহার করি:
+            int x = 10 + 5;
+            Console.WriteLine(x);  //  Outputs	:	15
+
+            /*
+                যদিও + অপারেটর সাধারণত দুটি মান যোগ করতে ব্যবহৃত হয়, যেমন উপরের উদাহরণে, 
+                এটি একটি ভেরিয়েবল এবং একটি মান, অথবা একটি ভেরিয়েবল এবং আরেকটি ভেরিয়েবল 
+                যোগ করতেও ব্যবহার করা যেতে পারে:
+            */
+            int sum1 = 10 + 5;              // 15 (10 + 5)
+            int sum2 = sum1 + 25;           // 40 (15 + 25)
+            int sum3 = sum2 + sum2;         // 80 (40 + 40)
+
+            Console.WriteLine(sum1);        //  Outputs	:	15	
+            Console.WriteLine(sum2);        //  Outputs	:	40
+            Console.WriteLine(sum3);        //  Outputs	:	80
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
